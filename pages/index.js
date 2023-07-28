5d4af05fc4cfd8faaf7a4bd8d226952cdef8d908
Oke <56229919+onissen@users.noreply.github.com>
2023-07-29 00:00:12 +0200

    shadow for jumbotron
    Einheitliches Design mit CDL

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ const Index = ({companies, persons, texts, jumbotron}) => {
 
   return(
     <Layout nopageHeader>
-      <div className="bg-white rounded-lg p-4 mt-8">
+      <div className="bg-white rounded-lg p-4 mt-8 shadow">
       <div className="mx-auto max-w-4xl">
         <h3 className="text-primary mb-3">{texts.attributes.headline}</h3>
         <div className="leading-relaxed text-justify" dangerouslySetInnerHTML={{ __html: jumbotron }}></div>
