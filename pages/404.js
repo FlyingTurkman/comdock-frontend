68ec42b68b0b4e9803a419ba88f0b492e9d28594
Oke <56229919+onissen@users.noreply.github.com>
2023-08-01 11:20:34 +0200

    next detected a class

--- pages/404.js
+++ pages/404.js
@@ -19,9 +19,9 @@ export default function NotFound() {
                 <div className="grow text-center justify-center items-center flex flex-col">
                     <AlertIcon theme="primary" weightClass='w-12' />
                     <div className="mt-10">
-                        <h4 class="text-primary">404</h4>
-                        <h1 class="mt-4">Seite nicht gefunden</h1>
-                        <p class="mt-6">Leider konnten wir die gewünschte Seite nicht finden.</p>
+                        <h4 className="text-primary">404</h4>
+                        <h1 className="mt-4">Seite nicht gefunden</h1>
+                        <p className="mt-6">Leider konnten wir die gewünschte Seite nicht finden.</p>
                         <div className="mt-10">
                             <Link href="/">
                                 <button className="btn btn-primary">Zurück zur Startseite</button>
