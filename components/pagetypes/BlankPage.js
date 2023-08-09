fa286f6aab13a6d084748a7c0a5118cfa8d73265
Oke <56229919+onissen@users.noreply.github.com>
2023-08-09 19:59:21 +0200

    BlankPage now populates title to PageHeader

--- components/pagetypes/BlankPage.js
+++ components/pagetypes/BlankPage.js
@@ -3,7 +3,7 @@ import PageHeader from "../basics/PageHeader";
 export default function BlankPage({title, children, noBreadcrumb}) {
     return (
         <>
-            <PageHeader noBreadcrumb={noBreadcrumb}>
+            <PageHeader noBreadcrumb={noBreadcrumb} title={title}>
                 <div className="h1">
                     {title}
                 </div>
